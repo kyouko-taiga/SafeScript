990610236b6d25c27d0172769e8fe907a75fc734
Dimitri Racordon <dimitri.racordon@gmail.com>
2018-04-25 11:34:10 +0200

    Add a transpiler target

--- Package.swift
+++ Package.swift
@@ -12,9 +12,10 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .target(name: "safescript", dependencies: ["Parser", "Sema"]),
+        .target(name: "safescript", dependencies: ["Parser", "Sema", "Transpiler"]),
         .target(name: "Parser", dependencies: ["AST"]),
         .target(name: "Sema", dependencies: ["AST", "Utils"]),
+        .target(name: "Transpiler", dependencies: ["AST"]),
         .target(name: "AST"),
         .target(name: "Utils"),
     ]
